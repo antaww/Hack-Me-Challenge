148730313af19aee3f59f58741e96f21c62535b5
antaww <anta.pizzetta@gmail.com>
2023-02-05 17:55:23 +0100

    feat(homepage): Add main container.

--- index.php
+++ index.php
@@ -18,5 +18,18 @@
 				<a href="challenge3.php">Challenge 3</a>
 			</button>
 		</div>
+
+		<div class="main">
+			<h1>Hack Me Challenge</h1>
+			<p>Your goal is to complete all the challenges in less than 30 minutes.</p>
+			<div class="difficulties">
+				<h2>Challenges difficulties</h2>
+				<ul>
+					<li>Challenge 1: 2/10</li>
+					<li>Challenge 2: 4/10</li>
+					<li>Challenge 3: 6/10</li>
+				</ul>
+			</div>
+		</div>
 	</body>
 </html>
