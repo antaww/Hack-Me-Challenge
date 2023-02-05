f02b6b8e2a91950a36c2e5a836c0ce15baec7865
antaww <anta.pizzetta@gmail.com>
2023-02-05 15:13:53 +0100

    feat(navbar): Add challenge selection on navbar.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang='en'>
+	<head>
+		<meta charset='UTF-8'>
+		<title>Hack Me Challenge</title>
+		<link rel='stylesheet' href='styles/index.css'>
+		<script src='scripts/index.js'></script>
+	</head>
+	<body>
+		<div class="navbar">
+			<button>
+				<a href="challenge1.php">Challenge 1</a>
+			</button>
+			<button>
+				<a href="challenge2.php">Challenge 2</a>
+			</button>
+			<button>
+				<a href="challenge3.php">Challenge 3</a>
+			</button>
+		</div>
+	</body>
+</html>
