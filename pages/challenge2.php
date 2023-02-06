c5baaf18a56fe6f233e2286cd0d630c55caa83b7
antaww <anta.pizzetta@gmail.com>
2023-02-06 16:18:01 +0100

    feat(style): Add challenge 2 security.

--- pages/challenge2.php
+++ pages/challenge2.php
@@ -1,43 +1,54 @@
 <!DOCTYPE html>
 <html lang='en'>
-<head>
-    <meta charset='UTF-8'>
-    <title>Hack Me Challenge</title>
-    <link rel='stylesheet' href='../styles/index.css'>
-    <link rel='stylesheet' href='../styles/challenge-2.css'>
-    <script src='../scripts/wtfisthis.js'></script>
-</head>
-<body>
-<div class="container">
-    <div class="pageTitle" style="color:white;">Challenge 2</div>
-    <?php
-    $host_name = 'db5011839211.hosting-data.io';
-    $database = 'dbs9971177';
-    $user_name = 'dbu5519438';
-    $password = '26032000Alexis/';
+	<head>
+		<meta charset='UTF-8'>
+		<title>Hack Me Challenge</title>
+		<link rel='stylesheet' href='../styles/index.css'>
+		<link rel='stylesheet' href='../styles/challenge-2.css'>
+		<script src='../scripts/wtfisthis.js'></script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="pageTitle" style="color:white;">Challenge 2</div>
+			<?php
+			$expectedCode = 'CHALLENGE48H'; //cookie -> message -> image -> flag js -> morse -> binary -> code
 
-    $link = new mysqli($host_name, $user_name, $password, $database);
+			if (!isset($_POST['codeC2'])) {
+				echo '<form autocomplete="off" class="password-form" method="post">';
+				echo '<label for="code">Enter code</label>';
+				echo '<input autocomplete="off" type="password" name="codeC2" id="code">';
+				echo '<button type="submit" value="Submit">Submit</button>';
+				echo '</form>';
+			} else {
+				$enteredCode = $_POST['codeC2'];
+				if ($enteredCode == $expectedCode) {
+					$host_name = 'db5011839211.hosting-data.io';
+					$database = 'dbs9971177';
+					$user_name = 'dbu5519438';
+					$password = '26032000Alexis/';
+					$link = new mysqli($host_name, $user_name, $password, $database); // Working on FTP only
 
-    $expectedCode = 'CHALLENGE48H'; //cookie -> message -> image -> flag js -> morse -> binary -> code
+					if (isset($_SERVER['QUERY_STRING']) && $_SERVER['QUERY_STRING'] == 'SELECT+*+FROM+dolphin') {
+						$sql = str_replace('+', ' ', $_SERVER['QUERY_STRING']);
+						$result = $link->query($sql);
 
-    if (isset($_SERVER['QUERY_STRING']) && $_SERVER['QUERY_STRING'] == 'SELECT+*+FROM+dolphin') {
-        $sql = str_replace("+", " ", $_SERVER['QUERY_STRING']);
-        $result = $link->query($sql);
+						if ($result->num_rows > 0) {
+							while ($row = $result->fetch_assoc()) {
+								echo 'id: ' . $row['id_animal'] . ' genre: ' . $row['gender'] . ' ' . $row['weight'] . '<br>';
+							}
+						}
+					} else {
+						echo 'What is my dolphin doing here ?';
+					}
 
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while ($row = $result->fetch_assoc()) {
-                echo "id: " . $row["id_animal"] . " genre: " . $row["gender"] . " " . $row["weight"] . "<br>";
-            }
-        }
-    } else {
-				echo 'What is my dolphin doing here ?';
+					echo '<img class="dolphin" src="dolphin.png" alt="dolphin">';
+				} else {
+					echo '<a href="challenge2.php"><button>Try again.</button></a>';
+				}
 			}
-
-			echo '<img class="dolphin" src="dolphin.png" alt="dolphin">'
 			?>
-</div>
-</body>
+		</div>
+	</body>
 </html>
 
 
